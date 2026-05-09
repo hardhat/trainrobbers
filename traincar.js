091b6264f574496433a0b3fcda1d28c9f68a76a3
DeathLemons <64559878+DeathLemons@users.noreply.github.com>
2026-05-09 19:55:29 -0400

    -made it so you can walk on the roof and the little outcrops next task to get ladders working

--- traincar.js
+++ traincar.js
@@ -19,18 +19,18 @@ export default class TrainCar extends Actor {
     createCollisionPlatforms() {
         const floorWidth = 146 * 3;
         const floorHeight = 1;
-        const roofWidth = 146 * 3;
+        const roofWidth = 146 * 2 + 30;
         const roofheight = 1;
 
-        this.createCollisionRectangle(this.x, this.y + 130, 19 * 3, floorHeight);
-        this.createCollisionRectangle(this.x + (floorWidth / 2) + 57, this.y + 160, floorWidth, floorHeight);
-        this.createCollisionRectangle(this.x + floorWidth + 57, this.y + 130, 19 * 3, floorHeight);
-
-        //this.createCollisionRectangle(this.X, this.y + 52, roofWidth, roofheight);
+        this.createCollisionRectangle(this.x + 5, this.y + 130, 19 * 3, floorHeight);
+        this.createCollisionRectangle(this.x, this.y + 160, 10000, floorHeight);
+        this.createCollisionRectangle(this.x + floorWidth + 130, this.y + 128, 1, 1);
+        this.createCollisionRectangle(this.x + 292, this.y + 52, roofWidth, 0.5);
+        console.log('x = ' + this.x + (floorWidth / 2));
     }
 
     createCollisionRectangle(x1, y1, width, height) {
-        let rect = this.scene.platforms.create(x1, y1, null).setSize(width, height).setOrigin(0, 0).setVisible(false).setAlpha(0);
+        let rect = this.scene.platforms.create(x1, y1, null).setSize(width, 1).setOrigin(0, 0).setVisible(false).setAlpha(0);
         this.scene.platforms.add(rect);
 
     }
